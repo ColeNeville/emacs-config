84f191541c7bac35178a809613fcc97e318c50e5
ColeNeville <colewneville2@gmail.com>
2024-04-05 19:47:25 -0600

    Restructuring my whole config.org file making it a little more self documenting

--- config.el
+++ config.el
@@ -7,61 +7,16 @@
 
 (auto-package-update-maybe)
 
-(use-package modus-themes
-  :ensure t
-  :custom
-  (modus-themes-common-palette-overrides
-   `((border-mode-line-active bg-mode-line-active)
-     (border-mode-line-inactive bg-mode-line-inactive)))
-  (modus-vivendi-palette-overrides
-   `((bg-main "#161616"))))
-
-(load-theme 'modus-vivendi t)
-
-(setq visible-bell t
-      ring-bell-function 'ignore)
-
-(setq inhibit-startup-screen t
-      inhibit-startup-message t)
-
-(setq display-line-numbers-minor-tick 5
-      display-line-numbers-major-tick 25)
-
-(setq-default display-line-numbers-width 4)
-
-(setq org-support-shift-select t
-      org-startup-truncated nil)
-
-(setq enable-recursive-minibuffers t)
-
-(setq custom-file "~/.config/custom.el")
-
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(tab-bar-mode -1)
-(line-number-mode -1)
-
-(menu-bar-mode 1)
-
-(add-hook 'prog-mode-hook 'flymake-mode)
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-
-(global-unset-key (kbd "C-x C-z"))
-
-(global-set-key [escape] 'keyboard-escape-quit)
-
 (define-prefix-command 'personal-prefix-map)
-(define-prefix-command 'personal-mode-toggle-prefix-map)
 
-(define-key personal-mode-toggle-prefix-map
-            "c" 'highlight-changes-mode)
-(define-key personal-mode-toggle-prefix-map
-            "w" 'whitespace-mode)
-
-(define-key personal-prefix-map
-            "m" 'personal-mode-toggle-prefix-map)
-
-(global-set-key (kbd "C-z") 'personal-prefix-map)
+(use-package modus-themes
+:ensure t
+:custom
+(modus-themes-common-palette-overrides
+ `((border-mode-line-active bg-mode-line-active)
+   (border-mode-line-inactive bg-mode-line-inactive)))
+(modus-vivendi-palette-overrides
+ `((bg-main "#161616"))))
 
 (use-package ace-window
   :ensure t
@@ -79,7 +34,6 @@
 
 (use-package treemacs
   :ensure t
-  :after (ivy counsel)
   :commands (treemacs
              treemacs-follow-mode
              treemacs-git-commit-diff-mode)
@@ -88,34 +42,10 @@
   :bind (:map personal-prefix-map
               ("t" . treemacs-select-window))
   :config
+  ;; These are "modes" but more specific to treemacs and its experience
+  ;; I am considering these as configuration for treemacs
   (treemacs-follow-mode 1)
   (treemacs-git-commit-diff-mode 1))
-
-;; Treemacs loads after ivy and counsel so the workspace
-;; picker has counsel support
-
-(use-package lsp-mode
-  :ensure t
-  :init
-  (setq lsp-keymap-prefix "C-z l")
-  :commands (lsp lsp-enable-which-key-integration)
-  :hook ((tsx-ts-mode . lsp)
-         (typescript-ts-mode . lsp)
-         (lsp-mode . lsp-enable-which-key-integration)))
-
-(use-package lsp-ui
-  :ensure t
-  :commands (lsp-ui-mode))
-
-(use-package lsp-ivy
-  :ensure t
-  :after (lsp-mode ivy)
-  :commands (lsp-ivy-workspace-symbol))
-
-(use-package lsp-treemacs
-  :ensure t
-  :after (lsp-mode treemacs)
-  :commands (lsp-treemacs-errors-list))
 
 (use-package ivy
   :ensure t
@@ -139,9 +69,29 @@
   :config
   (diminish 'counsel-mode))
 
-(ivy-mode 1)
-(counsel-mode 1)
-(treemacs 1) ;; Treemacs needs to be initialized after ivy and counsel
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-z l")
+  :commands (lsp lsp-enable-which-key-integration)
+  :hook ((tsx-ts-mode . lsp)
+         (typescript-ts-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration)))
+
+(use-package lsp-ui
+  :ensure t
+  :after (lsp-mode)
+  :commands (lsp-ui-mode))
+
+(use-package lsp-ivy
+  :ensure t
+  :after (lsp-mode ivy)
+  :commands (lsp-ivy-workspace-symbol))
+
+(use-package lsp-treemacs
+  :ensure t
+  :after (lsp-mode treemacs)
+  :commands (lsp-treemacs-errors-list))
 
 (use-package which-key
   :ensure t
@@ -150,8 +100,6 @@
   :custom
   (which-key-idle-delay 0.5)
   (diminish 'which-key-mode))
-
-(which-key-mode 1)
 
 (use-package company
   :ensure t
@@ -166,8 +114,6 @@
                        company-preview-if-just-one-frontend))
   :config
   (diminish 'company-mode))
-
-(global-company-mode 1)
 
 (use-package rainbow-delimiters
   :ensure t
@@ -192,9 +138,6 @@
   :ensure t
   :commands (global-treesit-auto-mode))
 
-(global-tree-sitter-mode)
-(global-treesit-auto-mode)
-
 (use-package ledger-mode
   :ensure t
   :mode ("\\.ledger\\'" "\\.journal\\'")
@@ -217,38 +160,24 @@
   :ensure t
   :mode ("\\.tf\\'"))
 
-(require 'org-tempo)
-
-(add-hook 'org-mode-hook (lambda () (org-indent-mode 1)))
-(eval-after-load 'org-indent '(diminish 'org-indent-mode))
-
-(add-to-list 'org-link-frame-setup '(file . find-file))
-
-(use-package toc-org
+(use-package robe
   :ensure t
-  :commands (toc-org-enable)
-  :hook ((org-mode . toc-org-enable)))
+  :hook ((ruby-mode . robe-mode)
+         (ruby-ts-mode . robe-mode)))
 
 (use-package org-bullets
   :ensure t
   :commands (org-bullets-mode)
   :hook ((org-mode . (lambda () (org-bullets-mode 1)))))
 
+(use-package toc-org
+  :ensure t
+  :commands (toc-org-enable)
+  :hook ((org-mode . toc-org-enable)))
+
 (define-prefix-command 'personal-org-roam-prefix-map)
 (define-key personal-prefix-map
             "n" 'personal-org-roam-prefix-map)
-
-;; ;; These aren't working as expected...
-;; (defun cn/org-roam-dailies-journal-today ()
-;;   "Create a journal entry in the today daily note"
-;;   (interactive)
-;;   (org-roam-dailies-capture-today :key "j"))
-
-;; (defun cn/org-roam-dailies-todo-today ()
-;;   "Create a todo entry in the today daily note"
-;;   (interactive)
-;;   (org-roam-dailies-capture-today :key "t"))
-;; ;;
 
 (use-package org-roam
   :ensure t
@@ -256,36 +185,31 @@
   :custom
   (org-roam-directory "~/notes/")
   (org-roam-capture-templates
-   '(("n" "note" plain "* %?"
-      :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                         "#+TITLE: ${title}")
-      :unnarrowed t)))
+   (let ((head "#+TITLE: ${title}")
+         (filename "%<%Y%m%d%H%M%S>-${slug}.org"))
+     `(("n" "note" plain "* %?"
+        :target (file+head ,filename ,head)
+        :unnarrowed t))))
   (org-roam-dailies-directory "daily/")
   (org-roam-dailies-capture-templates
    (let ((head "#+TITLE: %<%Y-%m-%d>\n\n* [/] Do today\n\n* Journal")
          (filename "%<%Y-%m-%d>.org"))
-     `(("j" "journal" item
-        "%<%H:%M> - %?"
+     `(("j" "journal" item "%<%H:%M> - %?"
         :target (file+head+olp ,filename ,head ("Journal"))
         :unarrowed t)
-       ("t" "todo" entry
-        "** TODO %?"
+       ("t" "todo" item "- [ ] %?"
         :target (file+head+olp ,filename ,head ("Do today"))
         :unarrowed t)
-       ("n" "note" entry
-        "* %?"
+       ("n" "note" entry "* %?"
         :target (file+head ,filename ,head)
         :unarrowed t)
        ("m" "meeting" entry
-        "* %{meeting_name}\n** Attending\n- %?\n** Notes\n*** \n** Takeaways [/]\n- [ ] "
+        "* %?\n** Attending\n- \n** Notes\n*** \n** Takeaways [/]\n- [ ] "
         :target (file+head ,filename ,head)
         :unarrowed t))))
   :commands (org-roam-setup)
   :bind (:map personal-org-roam-prefix-map
               ("b" . org-roam-buffer-toggle)
-              ;; Create a journal capture
-              ;; ("j" . cn/org-roam-dailies-journal-today)
-              ;; ("t" . cn/org-roam-dailies-todo-today)
               ;; (T)oday
               ("T" . org-roam-dailies-goto-today)
               ("t" . org-roam-dailies-capture-today)
@@ -314,9 +238,96 @@
 
 (use-package org-noter :ensure t)
 
+(use-package org-roam-ui :ensure t)
+
+(use-package org-ql
+  :ensure t)
+
+(use-package org-roam-ql
+  :ensure t
+  :after (org-roam))
+
+(use-package org-roam-ql-ql
+  :ensure t
+  :after (org-ql org-roam-ql))
+
+(require 'org-tempo)
+
+;; Remove the annoying ding on actions
+(setq visible-bell t
+      ring-bell-function 'ignore)
+
+;; Remove that annoying startup/welcome screen and just give me the scratchpad
+(setq inhibit-startup-screen t
+      inhibit-startup-message t)
+
+;; Configure the display line number on the left side of the buffer mode
+(setq display-line-numbers-minor-tick 5
+      display-line-numbers-major-tick 25)
+
+(setq-default display-line-numbers-width 4)
+
+;; Org mode options
+(setq org-support-shift-select t
+      org-startup-truncated nil)
+
+;; Opening a link between org notes should open in the same frame rather than a new one
+;; Frame spawning in emacs seems to be unpredictable (i need to look more into it)
+(add-to-list 'org-link-frame-setup '(file . find-file))
+
+;; This is nice when using ivy and counsel, but can also be a pain point
+(setq enable-recursive-minibuffers t)
+
+;; Move our custom files and keep init.el clean
+(setq custom-file "~/.config/emacs_custom.el")
+
+;; Disable some default keybindings -> These are things I don't use normally and will get hit accidentally
+(global-unset-key (kbd "C-x C-z"))
+
+;; Create my C-z prefix I use as my personal prefix
+;; -> This is used by some of the use-package definitions so has to happen in the pre init section
+(global-set-key (kbd "C-z") 'personal-prefix-map)
+
+;; Define a prefix that is useful for modes that can get in the way until you want them
+(define-prefix-command 'personal-mode-toggle-prefix-map)
+(define-key personal-prefix-map
+            "m" 'personal-mode-toggle-prefix-map)
+
+(define-key personal-mode-toggle-prefix-map
+            "c" 'highlight-changes-mode)
+(define-key personal-mode-toggle-prefix-map
+            "w" 'whitespace-mode)
+
+;; Programming modes hooks
+(add-hook 'prog-mode-hook 'flymake-mode)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(load-theme 'modus-vivendi t)
+
+;; Disable some default minor modes
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(line-number-mode -1)
+
+;; Enable some minor modes
+(menu-bar-mode 1)
+
+;; Enable some modes from packages above
+
+(global-company-mode 1)
+
+(which-key-mode 1)
+
+(ivy-mode 1)
+(counsel-mode 1)
+
+;; Treemacs needs to be initialized after ivy and counsel
+(treemacs 1)
+
+(global-tree-sitter-mode)
+(global-treesit-auto-mode)
+
 (org-roam-setup)
 
-(use-package robe
-  :ensure t
-  :hook ((ruby-mode . robe-mode)
-         (ruby-ts-mode . robe-mode)))
+(add-hook 'org-mode-hook (lambda () (org-indent-mode 1)))
+(eval-after-load 'org-indent '(diminish 'org-indent-mode))
